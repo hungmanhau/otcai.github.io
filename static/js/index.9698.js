39999b5a1bed497b6192cd3079798ea4cb956d94
hungdoba <hungdoba.hdb@gmail.com>
2022-05-17 12:47:43 +0900

    first commit

---
+++ static/js/index.9698.js
@@ -0,0 +1,9 @@
+var dest = "https://www.otcai.com/";
+var imageBanner = document.getElementById("imgBannerId");
+imageBanner.onclick = function (event) {
+  window.location.href = dest;
+};
+var background = document.getElementById("linkId");
+background.onclick = function (event) {
+  window.location.href = dest;
+};
