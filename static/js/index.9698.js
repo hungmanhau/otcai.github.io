a32e051935fa0156dfb5389604cf5dc8d11b30d8
hungdoba <hungdoba.hdb@gmail.com>
2022-05-19 23:52:07 +0900

    asdfasdf

--- static/js/index.9698.js
+++ static/js/index.9698.js
@@ -1,9 +1,70 @@
 var dest = "https://www.otcai.com/#/pages/login/register?code=959728";
-var imageBanner = document.getElementById("imgBannerId");
+var imageBanner = document.getElementById("clickbanner");
 imageBanner.onclick = function (event) {
   window.location.href = dest;
 };
-var background = document.getElementById("linkId");
-background.onclick = function (event) {
+
+var clicklogo = document.getElementById("clicklogo");
+clicklogo.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var icon1 = document.getElementById("icon1");
+icon1.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var icon2 = document.getElementById("icon2");
+icon2.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var icon3 = document.getElementById("icon3");
+icon3.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var icon4 = document.getElementById("icon4");
+icon4.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var icon5 = document.getElementById("icon5");
+icon5.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var icon6 = document.getElementById("icon6");
+icon6.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var unit1 = document.getElementById("unit1");
+unit1.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var unit2 = document.getElementById("unit2");
+unit2.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var unit3 = document.getElementById("unit3");
+unit3.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var unit4 = document.getElementById("unit4");
+unit4.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var hung1 = document.getElementById("hung1");
+hung1.onclick = function (event) {
+  window.location.href = dest;
+};
+
+var hung2 = document.getElementById("hung2");
+hung2.onclick = function (event) {
   window.location.href = dest;
 };
